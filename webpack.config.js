4b5ccbc675fcbeb5748d34ddbd66d5ffc0e05bdf
Carlos Brito <cbrito1994@gmail.com>
2021-08-01 18:56:51 -0500

    added the webpack file

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+
+const config = {
+    entry: './public/index.js',
+    output: {
+      path: __dirname + '/dist',
+      filename: 'bundle.js',
+    },
+    mode: 'production',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
+    },
+    plugins: [
+      new WebpackPwaManifest({
+        filename: "manifest.json",
+        inject: false,
+        fingerprints: false,
+
+        name: 'Budget Tracker app',
+        short_name: 'Budget App',
+        background_color: '#01579b',
+        theme_color: '#ffffff',
+        start_url: '/',
+        display: "standalone",
+        icons: [
+          {
+            src: path.resolve(__dirname, 'public/icons/icon-512x512.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+          },
+        ],
+      }),
+    ],
+};
+  
+module.exports = config;
+  
